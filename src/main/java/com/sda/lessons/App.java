140d212d137116a39b559b479e076ea2d782f167
Szymon Rybka <artifexet@gmail.com>
2019-03-23 10:25:24 +0000

    Start project

---
+++ src/main/java/com/sda/lessons/App.java
@@ -0,0 +1,24 @@
+package com.sda.lessons;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        int tab01[] = new int [3];
+         tab01[0] = 1;
+         tab01[1] = 2;
+         tab01[2] = 3;
+
+         int tab02[] = {1,2,3};
+
+
+
+
+    }
+}
